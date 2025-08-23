36beb22ed62d97a458faab0aff307c9b80357272
pankajsinghx6104 <pankajsinghx6104@gmail.com>
2025-08-23 11:02:30 +0530

    props and  tailwind

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -9,16 +9,16 @@ function App() {
     username: "pankaj",
     age: 21
   }
- // let myArray = [1, 2, 3, 4, 5]
+  //let myArray = [1, 2, 3, 4, 5]
 
    return (
     <>
     <h1 className="bg-red-400 text-3xl font-bold underline mb-6">
       Hello world!!!!
     </h1>
-    <Card username = "chaiaurcode"someObj={myObj} />
+    <Card username = "chaiaurcode"someObj={myObj} btnText="learn more"/>
     <Card username = "pankaj"btnText="Click me"/>
-    <Card username = "Kuldeep" btnText="Visit Me"/>
+    <Card username = "Kuldeep" btnText="Visit"/>
     </>
   )
 }
